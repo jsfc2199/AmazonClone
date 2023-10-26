2dfbcac7596bcc4ca4005a09add5d6248c40e4b4
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2023-10-25 20:50:06 -0500

    final setup for tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,31 +1,37 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Producto} from './model/products.model';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'AmazonClone';
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {
-    this.fetchProducts();
+  ngOnInit(): void {
+    initFlowbite();
   }
 
-  fetchProducts() {
-    this.http
-      .get<Producto[]>(
-        'https://clone-a290a-default-rtdb.firebaseio.com/productos.json'
-      )
-      .subscribe((product) => {
-        product.forEach((item, i) => {
-          console.log(i + 1, item.product_result.title);
-        });
-      });
-  }
+
+  // ngOnInit() {
+  //   this.fetchProducts();
+  // }
+
+  // fetchProducts() {
+  //   this.http
+  //     .get<Producto[]>(
+  //       'https://clone-a290a-default-rtdb.firebaseio.com/productos.json'
+  //     )
+  //     .subscribe((product) => {
+  //       product.forEach((item, i) => {
+  //         console.log(i + 1, item.product_result.title);
+  //       });
+  //     });
+  // }
 
 }
